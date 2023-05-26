01a5df8aaabc7d36c27c13309a728047c3c7e74b
Leonard <lemrcedes030@gmail.com>
2023-05-26 12:02:40 -0400

    Update to my-app2

--- src/Employees.js
+++ src/Employees.js
@@ -18,44 +18,44 @@ export const Employees = () => {
         <table>
             <th>Employees...</th>
             <tr>
-                <td>{ employees[0]?.name}</td> 
-                <td>{ employees[0]?.email}</td>
+                <td>{ employees[0]?.name }</td> 
+                <td>{ employees[0]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[1]?.name}</td> 
-                <td>{ employees[1]?.email}</td>
+                <td>{ employees[1]?.name }</td> 
+                <td>{ employees[1]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[2]?.name}</td> 
-                <td>{ employees[2]?.email}</td>
+                <td>{ employees[2]?.name }</td> 
+                <td>{ employees[2]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[3]?.name}</td> 
-                <td>{ employees[3]?.email}</td>
+                <td>{ employees[3]?.name }</td> 
+                <td>{ employees[3]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[4]?.name}</td> 
-                <td>{ employees[4]?.email}</td>
+                <td>{ employees[4]?.name }</td> 
+                <td>{ employees[4]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[5]?.name}</td> 
-                <td>{ employees[5]?.email}</td>
+                <td>{ employees[5]?.name }</td> 
+                <td>{ employees[5]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[6]?.name}</td> 
-                <td>{ employees[6]?.email}</td>
+                <td>{ employees[6]?.name }</td> 
+                <td>{ employees[6]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[7]?.name}</td> 
-                <td>{ employees[7]?.email}</td>
+                <td>{ employees[7]?.name }</td> 
+                <td>{ employees[7]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[8]?.name}</td> 
-                <td>{ employees[8]?.email}</td>
+                <td>{ employees[8]?.name }</td> 
+                <td>{ employees[8]?.email }</td>
             </tr>
             <tr>
-                <td>{ employees[9]?.name}</td> 
-                <td>{ employees[9]?.email}</td>
+                <td>{ employees[9]?.name }</td> 
+                <td>{ employees[9]?.email }</td>
             </tr>
         </table>
     </div>
